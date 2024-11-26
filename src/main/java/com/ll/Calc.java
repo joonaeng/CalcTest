c79e77ab661fd411a4ccd525a30f0aff4da33145
joonaeng <jylee33286315@gmail.com>
2024-11-26 12:40:57 +0900

    work

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,8 +1,8 @@
 package com.ll;
 
-public class Calc {
-    public static int run(String expr) {
-        String[] exprBits = expr.split("//+");
-
-    }
-}
+//public class Calc {
+//    public static int run(String expr) {
+//        String[] exprBits = expr.split("//+");
+//
+//    }
+//}
